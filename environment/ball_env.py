1c756c47c500ce7a8c88267048dd7075fe47be11
HulbertAlejandro <hulberta.arangof@uqvirtual.edu.co>
2026-09-02 21:21:07 -0500

    Añadir gravedad a la dinámica de la bola

---
+++ environment/ball_env.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+
+class BallEnv:
+    def __init__(self):
+        self.width = 800
+        self.height = 600
+        self.gravity = 0.5
+
+        self.x = 0.0
+        self.y = 0.0
+        self.vx = 0.0
+        self.vy = 0.0
+
+    def reset(self):
+        self.x = 100.0
+        self.y = 100.0
+        self.vx = 0.0
+        self.vy = 0.0
+
+        return self.get_state()
+
+    def get_state(self):
+        return np.array(
+            [self.x, self.y, self.vx, self.vy],
+            dtype=np.float32
+        )
+
+    def step(self):
+        self.vy += self.gravity
+
+        self.x += self.vx
+        self.y += self.vy
+
+        return self.get_state()
